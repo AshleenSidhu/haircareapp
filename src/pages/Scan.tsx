b4825a19b6b30c1517b5bc56759587096587f4cf
Ashleen Sidhu <Ssidhuash4@gmail.com>
2025-11-20 16:24:02 -0700

    change nav

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -20,7 +20,7 @@ const Scan = () => {
 
   // Simple label list for classification
   // (Replace with your actual hair type labels)
-  const labels = ["Straight", "Wavy", "Curly", "Coily", "Dreadlocks"];
+  const labels = ["Straight", "Wavy", "Curly", "Kinky", "Dreadlocks"];
 
   // Redirect user if quiz is already done
   useEffect(() => {
